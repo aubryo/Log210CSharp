38a2409c6bb9e59bc82be80f4d5ce983c00ce4ea
bambooPlus <cplgagnonm@hotmail.com>
2014-07-14 08:34:55 -0400

    Controleur menus

---
+++ GestionnaireLivraison/controleur/ControleurMenus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GestionnaireLivraison.model;
+
+namespace GestionnaireLivraison.controleur
+{
+    public class ControleurMenus
+    {
+        public static Menu ajouterMenu(Restaurant restaurant)
+        {
+            return new Menu() { RestaurantId = restaurant.Id };
+        }
+
+        public static void enregistrerMenu(Menu menu, List<Plat> plats)
+        {
+            //TODO menu.Insert();
+            foreach (Plat p in plats)
+            {
+                p.MenuId = menu.Id;
+                //TODO p.Update();
+            }
+        }
+    }
+}

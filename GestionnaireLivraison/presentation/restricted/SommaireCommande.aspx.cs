dc6f80734cb669d7fbc30fd8bbf76c55ab525950
bambooPlus <cplgagnonm@hotmail.com>
2014-07-15 23:53:22 -0400

    sommairecommande

---
+++ GestionnaireLivraison/presentation/restricted/SommaireCommande.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GestionnaireLivraison.presentation.restricted
+{
+    public partial class SommaireCommande : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string commandeId = Request.QueryString["Id"];
+            if (!IsPostBack)
+            {
+                model.Commande commande = new model.Commande() { Id = Request.QueryString["Id"].ToObjectId() };
+                commande.Select();
+
+                lblNoCommande.Text = commande.NoCommande;
+                hlRetourAccueil.NavigateUrl = "~/presentation/restricted/AccueilClient.aspx.cs?Id=" + commande.ClientId;
+            }
+        }
+    }
+}
